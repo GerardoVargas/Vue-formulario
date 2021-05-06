32c4cabbc69d8de09500722dc9609debb747cada
Gerardo Vargas <evc414@gmail.com>
2021-05-06 14:10:14 -0500

    iniciando app vue, se edita el componente boton personalizado

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Bienvenido a una App con Vue.js"/>
-    <Prueba />
+    <Button 
+      color='white'
+      background='darkslateblue'
+      isDisabled='true'
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Prueba from './components/Prueba.vue'
+import Button from './components/Button.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Prueba
+    Button
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,48 @@
+/* 
+  EN UN SFC (Single File Component) se trabaja en 3 secciones:
+  - Template
+  - Script
+  - Styles
+ */
+
+<template>
+  <button 
+    :style="{ background, color }"
+  >
+    Prueba
+  </button>
+</template>
+
+<script>
+
+export default {
+  name: 'Button',
+  props:{
+    color: {
+      type: String
+    },
+    background: {
+      type: String
+    },
+    isDisabled: {
+      type: Boolean
+    }
+  }
+}
+</script>
+ 
+ <style scoped>
+  button {
+    background: none;
+    color: black;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 40px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+
+  button:hover {
+    filter: brightness(125%);
+  }
+ </style>
